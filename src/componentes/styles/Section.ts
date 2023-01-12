f016071e9645d830a281e14737ff2097b307eb93
André Carlos Jr <andre-carlos@live.com>
2023-01-11 22:38:35 -0300

    - fix responsive

--- src/componentes/styles/Section.ts
+++ src/componentes/styles/Section.ts
@@ -1,16 +1,15 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
-  height: 61vh;
+  height: 55vh;
+  width: 100%;
   border-top-right-radius: 25%;
   border-top-left-radius: 25%;
   background: #fff9eb;
   border: 2px solid black;
   padding-top: 50px;
-  position: absolute;
-  bottom: -110px;
-  left: 0;
-  right: 0;
+  position: relative;
+  align-self: flex-end;
 
   &:after {
     content: "";
@@ -20,9 +19,25 @@ export const Section = styled.section`
     background-size: 328px 158px;
     display: block;
     position: absolute;
-    top: -45%;
+    top: -142px;
     left: 0;
     right: 0;
     margin: 0 auto;
+  }
+
+  @media (min-width: 768px) {
+    &:after {
+      content: "";
+      width: 360px;
+      height: 168px;
+      background-image: url("./imagens/participante.png");
+      background-size: 360px 168px;
+      display: block;
+      position: absolute;
+      top: -150px;
+      left: 170px;
+      right: 0;
+      margin: 0 auto;
+    }
   }
 `;
